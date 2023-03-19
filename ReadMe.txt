bb553d40b153add91ac21589cc69a8c559095202
mark <markpackham1@gmail.com>
2023-03-19 08:32:18 +0000

    setup user interface
    https://odysee.com/@WebDevSimplified:8/how-to-build-your-own-ai-with-chatgpt:d?r=7FU2nojSFqrsu4QQATzPqtwMSeVpNiGR&t=470

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,5 +1,9 @@
 Project requires a .env with a secret key purchased from the OpenAI API, prices rise depending on usage
+https://platform.openai.com/account/api-keys
 
 API_KEY=
 
 Tutorial taken from https://odysee.com/@WebDevSimplified:8/how-to-build-your-own-ai-with-chatgpt:d
+
+This tutorial shows how to use it with Python
+https://platform.openai.com/docs/api-reference/authentication
--- script.js
+++ script.js
@@ -2,12 +2,22 @@ import { config } from "dotenv";
 config();
 
 import { Configuration, OpenAIApi } from "openai";
+// readline is built into node.js
+import readline from "readline";
 
 const openai = new OpenAIApi(
   new Configuration({
     apiKey: process.env.API_KEY,
   })
 );
+
+// take user's input and process the standard input and output
+const userInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+userInterface.prompt();
 
 openai
   .createChatCompletion({
