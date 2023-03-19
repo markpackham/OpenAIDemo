3bd43d4fbd75d7243a7cd2ec0e2e5febb2608fa8
mark <markpackham1@gmail.com>
2023-03-19 08:38:16 +0000

    put user input in message
    https://odysee.com/@WebDevSimplified:8/how-to-build-your-own-ai-with-chatgpt:d?r=7FU2nojSFqrsu4QQATzPqtwMSeVpNiGR&t=516

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config();
 
 import { Configuration, OpenAIApi } from "openai";
-// readline is built into node.js
+// Readline is built into node.js
 import readline from "readline";
 
 const openai = new OpenAIApi(
@@ -11,7 +11,7 @@ const openai = new OpenAIApi(
   })
 );
 
-// take user's input and process the standard input and output
+// Take user's input and process the standard input and output
 const userInterface = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -19,14 +19,15 @@ const userInterface = readline.createInterface({
 
 userInterface.prompt();
 
-// add listener, reacts to everytime Enter/Return key is hit so "line" is changed
+// Add listener, reacts to everytime Enter/Return key is hit so "line" is changed
 userInterface.on("line", async (input) => {
-  openai
-    .createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: "Hello ChatGPT" }],
-    })
-    .then((res) => {
-      console.log(res.data.choices[0].message.content);
-    });
+  const res = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: input }],
+  });
+  console.log(res.data.choices[0].message.content);
+
+  // .then((res) => {
+  //   console.log(res.data.choices[0].message.content);
+  // });
 });
